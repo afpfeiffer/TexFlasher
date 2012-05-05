178c67aa17386d82afef8421824d1c7031027be3
Can Oezmen <can@mithrandir.mathematik.uni-freiburg.de>
2012-05-05 13:32:13 +0200

    small change

--- install.sh
+++ install.sh
@@ -17,6 +17,7 @@
 #     You should have received a copy of the GNU General Public License
 #     along with TexFlasher  If not, see <http://www.gnu.org/licenses/>.
 
+
 clear
 
 python .TexFlasher/credits.py
