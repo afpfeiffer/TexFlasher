1a2c00b364f98d882ef8437835b76420b6af561d
Axel Pfeiffer <afpfeiffer@googlemail.com>
2012-05-19 12:39:22 +0200

    add some comments

--- .TexFlasher/scripts/save.sh
+++ .TexFlasher/scripts/save.sh
@@ -38,6 +38,32 @@ files=$*
 
 txtrst=$(tput sgr0) # Text reset
 txtred=$(tput setaf 1) # Red
+txtbgw=$(tput setb 7) #white background
+
+# Other variables you can define as follows:
+# txtgrn=$(tput setaf 2) # Green
+# txtylw=$(tput setaf 3) # Yellow
+# txtblu=$(tput setaf 4) # Blue
+# txtpur=$(tput setaf 5) # Purple
+# txtcyn=$(tput setaf 6) # Cyan
+# txtwht=$(tput setaf 7) # White
+# txtrst=$(tput sgr0) # Text reset
+# Following are the tput details further:
+# tput setab [1-7] : Set a background colour using ANSI escape
+# tput setb [1-7] : Set a background colour
+# tput setaf [1-7] : Set a foreground colour using ANSI escape
+# tput setf [1-7] : Set a foreground colour
+# 
+# tput Text Mode Capabilities:
+# 
+# tput bold : Set bold mode
+# tput dim : turn on half-bright mode
+# tput smul : begin underline mode
+# tput rmul : exit underline mode
+# tput rev : Turn on reverse mode
+# tput smso : Enter standout mode (bold on rxvt)
+# tput rmso : Exit standout mode
+# tput sgr0 : Turn off all attributes (doesn’t work quite as expected)
 
 for thing in $files; do
 # 	echo $thing
@@ -54,7 +80,7 @@ for thing in $files; do
 			if [ "$fulldiff" == "" ]; then			
 				echo "  -> $files"
 			else
-				echo "  -> ${txtred}$files ${txtrst} "
+				echo "  -> ${txtred}${txtbgw}$files ${txtrst} "
 			fi
 		done
 		echo
