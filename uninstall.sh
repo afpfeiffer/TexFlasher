0fb64693b5e62a320848962b4a7a5c0b7ba92b70
Axel Pfeiffer <afpfeiffer@googlemail.com>
2012-06-12 00:58:05 +0200

    ntroduce uninstall.sh

---
+++ uninstall.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+rm $HOME/Desktop/TexFlasher.desktop
+rm $HOME/.local/share/applications/TexFlasher.desktop
+rm -rf $HOME/TexFlasher/.TexFlasher
+rm $HOME/TexFlasher/install.sh
+rm $HOME/TexFlasher/settings
+rm $HOME/TexFlasher/run-TexFlasher.sh
+
+exit 0
