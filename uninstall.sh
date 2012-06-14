edf00acd60f201176948e7c9dff7f55c08b2ac54
Axel Pfeiffer <afpfeiffer@googlemail.com>
2012-06-14 06:56:31 +0200

    forgot on thing

--- uninstall.sh
+++ uninstall.sh
@@ -8,4 +8,9 @@ rm install.sh
 rm settings
 rm run-TexFlasher.sh
 rm uninstall.sh
+startupscript=/usr/local/bin/texflasher
+if [ -f $startupscript ]; then
+	echo "Need root rights to remove $startupscript"
+	sudo rm $startupscript
+fi
 exit 0
