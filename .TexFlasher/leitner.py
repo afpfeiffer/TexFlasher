e39d48017e04e03bc4942f06d9f318a06be44896
Can Oezmen <can@mithrandir.mathematik.uni-freiburg.de>
2012-06-29 14:23:22 +0200

    fix

--- .TexFlasher/leitner.py
+++ .TexFlasher/leitner.py
@@ -1499,7 +1499,7 @@ class Flasher:
 		if pagemarker==None:
 			pagemarker="unset"
 		print self.c.order.getElementsByTagName(flashcard_name)[0].getAttribute("position")
-		fc_pos=1#int(self.c.order.getElementsByTagName(flashcard_name)[0].getAttribute('position'))
+		fc_pos=int(self.c.order.getElementsByTagName(flashcard_name)[0].getAttribute('position'))
 	
 		self.c.fc_det_left.set("Flashcards (left / total): "+str(flashcardsTodo-listPosition)+" / "+str(totalNumberCards))	
 	   	self.c.fc_det_right.set("Tag: "+flashcard_name+", Nr.: "+str(fc_pos)+", Page: "+str(pagemarker))
