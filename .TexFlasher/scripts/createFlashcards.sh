45676e3548afcb3ebec5922092023a308b88df27
Axel Pfeiffer <afpfeiffer@googlemail.com>
2012-07-18 21:10:52 +0200

    stop crap

--- .TexFlasher/scripts/createFlashcards.sh
+++ .TexFlasher/scripts/createFlashcards.sh
@@ -282,7 +282,7 @@ echo
 	make -i -j$procs images 2>&1 < /dev/null | grep -rniE 'compiled flashcard|error|ERROR|Error|Missing|Emergency stop.|Undefined control sequence' | tee -a $folder/texFlasher.log
 	
 	cd $folder/Diffs
-	make -i -j$procs images 2>&1 < /dev/null 
+	make -i -j$procs images 2>&1 < /dev/null &> /dev/null
 done
 	
 exit 0
