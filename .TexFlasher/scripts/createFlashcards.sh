488fe6e220bc1e51f9791fda514d48c744573c33
Axel Pfeiffer <axel.pfeiffer@mathematik.uni-freiburg.de>
2012-07-18 18:07:03 +0200

    fix shit

--- .TexFlasher/scripts/createFlashcards.sh
+++ .TexFlasher/scripts/createFlashcards.sh
@@ -278,8 +278,11 @@ echo
 
 # 	cd $folder/Diffs
 	# better be save than sorry
-	make -i -j$procs images 2>&1 < /dev/null | grep -rniE 'compiled flashcard|error|ERROR|Error|Missing|Emergency stop.|Undefined control sequence' | tee -a $folder/texFlasher.log
 	cd $WD
+	make -i -j$procs images 2>&1 < /dev/null | grep -rniE 'compiled flashcard|error|ERROR|Error|Missing|Emergency stop.|Undefined control sequence' | tee -a $folder/texFlasher.log
+	
+	cd $folder/Diffs
+	make -i -j$procs images 2>&1 < /dev/null 
 done
 	
 exit 0
